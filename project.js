9a9c94ad2a9c14208dce9ad1bd765d7cb6e535b2
Will13249 <82103462+Will13249@users.noreply.github.com>
2021-04-08 14:11:24 -0400

    Update project.js

--- project.js
+++ project.js
@@ -1,2 +1,44 @@
+var index = 0;
+var countries;
+
+countries = ["China", "UnitedStates", "France", "Somalia", "Germany", "Argentina", "Canada", "Russia"];
+             
+
+
+
+
+
+
+onEvent("button1", "click", function( ) {
+  setScreen("screen2");
+  updateScreen();
+  updateText();
+  updateText2();
+  updateText3();
+  updateText4();
+});
+
+function updateScreen() {
+  index = randomNumber(0, countries.length - 1);
+  setImageURL("image2", countries[index] + ".jpg"); 
+  console.log(countries[index]);
+}
+
+function updateText() {
+  index = randomNumber(0, countries.length -1);
+  setProperty("button2", "text", countries[index]);
+}
+function updateText2() {
+  index = randomNumber(0, countries.length -1);
+  setProperty("button3", "text", countries[index]);
+}
+function updateText3() {
+  index = randomNumber(0, countries.length -1);
+  setProperty("button4", "text", countries[index]);
+}
+function updateText4() {
+  index = randomNumber(0, countries.length -1);
+  setProperty("button5", "text", countries[index]);
+}
 
 // This is my project repository file
