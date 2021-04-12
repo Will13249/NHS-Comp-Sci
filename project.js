ec92266bdcad2dcbd995a3c685996e55e627f71a
Will13249 <82103462+Will13249@users.noreply.github.com>
2021-04-12 14:11:32 -0400

    Update project.js

--- project.js
+++ project.js
@@ -1,11 +1,9 @@
 var index = 0;
 var countries;
+var timeleft = 10;
 
 countries = ["China", "UnitedStates", "France", "Somalia", "Germany", "Argentina", "Canada", "Russia"];
              
-
-
-
 
 
 
@@ -21,6 +19,7 @@ onEvent("button1", "click", function( ) {
 function updateScreen() {
   index = randomNumber(0, countries.length - 1);
   setImageURL("image2", countries[index] + ".jpg"); 
+  setImageURL("image3", countries[index] + ".jpg");
   console.log(countries[index]);
 }
 
@@ -40,5 +39,23 @@ function updateText4() {
   index = randomNumber(0, countries.length -1);
   setProperty("button5", "text", countries[index]);
 }
+
+onEvent("label7", "click", function( ) {
+  setScreen("screen5");
+});
+onEvent("label8", "click", function( ) {
+  setScreen("screen5");
+});
+
+
+timedLoop(1000, function() {
+  timeleft = timeleft-1;
+  setText("Timer", "Time:" + timeleft);
+if (timeleft == 0) {
+ stopTimedLoop();
+ setScreen("screen4");
+}
+});
+
 
 // This is my project repository file
