2c8e819d12f812191a9612698eca441700125be9
Will13249 <82103462+Will13249@users.noreply.github.com>
2021-04-15 14:09:32 -0400

    Update project.js

--- project.js
+++ project.js
@@ -1,50 +1,37 @@
 var index = 0;
+var indx;
 var countries;
 var timeleft = 10;
+var correctanswer;
 
 countries = ["China", "UnitedStates", "France", "Somalia", "Germany", "Argentina", "Canada", "Russia"];
-             
+var buttons = ["button2", "button3", "button4", "button5"];     
 
 
 
 onEvent("button1", "click", function( ) {
   setScreen("screen2");
   updateScreen();
-  updateText();
-  updateText2();
-  updateText3();
-  updateText4();
 });
 
 function updateScreen() {
   index = randomNumber(0, countries.length - 1);
-  setImageURL("image2", countries[index] + ".jpg"); 
-  setImageURL("image3", countries[index] + ".jpg");
-  console.log(countries[index]);
+  setImageURL("image2", countries[index] + ".jpg");
+  correctanswer = countries[index];
+  indx = randomNumber(0, buttons.length - 1);
+  setProperty(buttons[indx], "text", countries[index]);
 }
+onEvent(buttons[indx], "click", function( ) {
+  setScreen("screen3");
+});
 
-function updateText() {
-  index = randomNumber(0, countries.length -1);
-  setProperty("button2", "text", countries[index]);
-}
-function updateText2() {
-  index = randomNumber(0, countries.length -1);
-  setProperty("button3", "text", countries[index]);
-}
-function updateText3() {
-  index = randomNumber(0, countries.length -1);
-  setProperty("button4", "text", countries[index]);
-}
-function updateText4() {
-  index = randomNumber(0, countries.length -1);
-  setProperty("button5", "text", countries[index]);
-}
+
 
 onEvent("label7", "click", function( ) {
-  setScreen("screen5");
+  setScreen("screen2");
 });
 onEvent("label8", "click", function( ) {
-  setScreen("screen5");
+  setScreen("screen2");
 });
 
 
