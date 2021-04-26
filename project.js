378c076093142805e007d7b7dfc58da030588ea7
Will13249 <82103462+Will13249@users.noreply.github.com>
2021-04-26 14:09:36 -0400

    Update project.js

--- project.js
+++ project.js
@@ -1,8 +1,13 @@
 var index = 0;
 var indx;
+var ind;
+var wrong1;
+var wrong2;
+var wrong3;
 var countries;
 var timeleft = 10;
 var correctanswer;
+var correctbutton;
 
 countries = ["China", "UnitedStates", "France", "Somalia", "Germany", "Argentina", "Canada", "Russia"];
 var buttons = ["button2", "button3", "button4", "button5"];     
@@ -20,12 +25,21 @@ function updateScreen() {
   correctanswer = countries[index];
   indx = randomNumber(0, buttons.length - 1);
   setProperty(buttons[indx], "text", countries[index]);
+  correctbutton = buttons[indx];
+  wrong1 = randomNumber(0, countries.length - 1);
+  wrong2 = randomNumber(0, countries.length - 1);
+  wrong3 = randomNumber(0, countries.length - 1);
+  while(wrong1 == countries[index]){
+    wrong1 = randomNumber(0, countries.length - 1);
+  }
+  while(wrong2 == countries[index] || wrong2 == wrong1) {
+    wrong2 = randomNumber(0, countries.length - 1);
+  }
+  while(wrong3 == countries[index] || wrong3 == wrong2 == wrong3 == wrong2) {
+    wrong3 = randomNumber(0, countries.length - 1);
+  }
+  setProperty("button2", "text", wrong1);
 }
-onEvent(buttons[indx], "click", function( ) {
-  setScreen("screen3");
-});
-
-
 
 onEvent("label7", "click", function( ) {
   setScreen("screen2");
